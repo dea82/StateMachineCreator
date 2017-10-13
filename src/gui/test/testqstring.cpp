deb4f62e01d9d9dbe806100dedfc1e21d41b5004
Andreas Laghamn <andreas.laghamn@gmail.com>
2017-10-13 18:52:10 +0200

    Dev switch

---
+++ src/gui/test/testqstring.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Andreas Laghamn on 2017-10-10.
+//
+
+#include <QtTest/QtTest>
+
+class TestQString: public QObject
+{
+ Q_OBJECT
+ private slots:
+  void toUpper();
+};
+
+void TestQString::toUpper()
+{
+  QString str = "Hello";
+  QCOMPARE(str.toUpper(), QString("HELLO"));
+}
+
+QTEST_MAIN(TestQString)
+#include "testqstring.moc"
