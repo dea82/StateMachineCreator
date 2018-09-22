3e29cb601bde9b6c1d0f5e2250262658e2e443e5
Andreas <andreas.laghamn@gmail.com>
2018-09-23 00:01:38 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # StateMachineCreator
-State machine creator
+[![Build Status](https://travis-ci.org/dea82/StateMachineCreator.svg?branch=develop)](https://travis-ci.org/dea82/StateMachineCreator)
 [![codecov](https://codecov.io/gh/dea82/StateMachineCreator/branch/develop/graph/badge.svg)](https://codecov.io/gh/dea82/StateMachineCreator)
--- build-tools/upload_codecov.py
+++ build-tools/upload_codecov.py
@@ -10,13 +10,10 @@ def main():
     parser.add_argument('-t', '--target', action='store', required=True, help="Target to generate coverage files.")
     args = parser.parse_args()
 
-    print("Trying to build coverage target in directory: " + args.build_path + " Target: " + args.target)
-    os.system("echo $SHELL")
     os.chdir(args.build_path)
     os.system("cmake --build " + args.build_path + " -- " + args.target)
     os.system("lcov --directory . --capture --output-file coverage.info")
     os.system("lcov --remove coverage.info '/usr/*' --output-file coverage.info")
-    #os.system("lcov --remove coverage.info '/home/conan/.conan/*' --output-file coverage.info")
     os.system("lcov --list coverage.info")
     os.system('/bin/bash -c "bash <(curl -s https://codecov.io/bash) || echo \"Codecov did not collect coverage reports\""')
 
